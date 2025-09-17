8611837797a11584562ac6d8b7b4434b37ddcef2
Deepak Verma <dpakverma789@gmail.com>
2025-09-17 09:00:53 +0530

    show box

--- django_project/showbox/models.py
+++ django_project/showbox/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class ShowDetails(models.Model):
-    name = models.CharField()
-    show_time = models.CharField()
+    name = models.CharField(max_length=100)
+    show_time = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 
 class TheaterDetails(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     capacity = models.IntegerField()
     reserved_seats = models.IntegerField()
     unreserved_seats = models.IntegerField()
@@ -19,7 +19,7 @@ class TheaterDetails(models.Model):
 
 
 class BookingDetails(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     reserved_seats = models.IntegerField()
     amount = models.IntegerField(default=0)
     theater_details = models.ForeignKey(TheaterDetails, on_delete=models.CASCADE,db_column="theater_details_id") # M2O
